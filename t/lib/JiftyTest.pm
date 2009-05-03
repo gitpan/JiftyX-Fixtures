74dcd6c0ae724e408429fe29bef8a4ae3dccabd6
許家瑋 <shelling@cpan.org>
2009-05-03 11:25:55 +0000

    Import of SHELLING/JiftyX-Fixtures-0.01 from CPAN.
    gitpan-cpan-distribution: JiftyX-Fixtures
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHELLING/JiftyX-Fixtures-0.01.tar.gz
    gitpan-cpan-author:       SHELLING
    gitpan-cpan-maturity:     released

---
+++ t/lib/JiftyTest.pm
@@ -0,0 +1,15 @@
+package JiftyTest;
+our $VERSION = '0.01';
+
+
+use File::Basename;
+use Cwd 'abs_path';
+
+$ENV{'JIFTY_APP_ROOT'} = dirname( abs_path(__FILE__) ) . "/JiftyTest";
+$ENV{'JIFTY_APP_ROOT'} = "/Users/shelling/develope/perl/JiftyX-Fixtures/t/lib/JiftyTest";
+
+sub start {
+  Jifty->web->add_javascript("main.js");
+}
+
+1;
---
+++ t/lib/JiftyTest/Model/Session.pm
@@ -0,0 +1,9 @@
+package JiftyTest::Model::Session;
+our $VERSION = '0.01';
+
+
+use base Jifty::Model::Session;
+
+
+
+1;
